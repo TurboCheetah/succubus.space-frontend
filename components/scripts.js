f2bd885c35bab0b6d02b849ab396802374f20a2c
Turbo <me@turbo.ooo>
2021-07-30 03:06:05 -0500

    feat: remove useless scripts

--- components/scripts.js
+++ components/scripts.js
@@ -2,11 +2,7 @@ const Scripts = () => (
   <div>
     <script src='/js/jquery-3.3.1.min.js' />
     <script src='/js/bootstrap.min.js' />
-    <script src='/js/player.js' />
-    <script src='/js/jquery.nice-select.min.js' />
-    <script src='/js/mixitup.min.js' />
     <script src='/js/jquery.slicknav.js' />
-    <script src='/js/owl.carousel.min.js' />
     <script src='/js/main.js' />
   </div>
 )
