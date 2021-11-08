b79f66988ff7c1aedba84161413ac0729deaf14e
turbo <me@turbo.ooo>
2021-11-07 19:27:28 -0500

    fix: proper linting

--- components/scripts.js
+++ components/scripts.js
@@ -1,9 +1,9 @@
 const Scripts = () => (
   <div>
-    <script src='/js/jquery-3.3.1.min.js' />
-    <script src='/js/bootstrap.min.js' />
-    <script src='/js/jquery.slicknav.js' />
-    <script src='/js/main.js' />
+    <script src="/js/jquery-3.3.1.min.js" />
+    <script src="/js/bootstrap.min.js" />
+    <script src="/js/jquery.slicknav.js" />
+    <script src="/js/main.js" />
   </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,7 @@ import Layout from '../components/layout'
 
 const Index = () => (
   <div>
-    <Layout
-      title="Succubus.Space"
-      description="Have you ever wanted to find hentai? Succubus.Space puts it all in one elegant website!"
-      image="img/logo.png"
-    />
+    <Layout title="Succubus.Space" description="Have you ever wanted to find hentai? Succubus.Space puts it all in one elegant website!" image="img/logo.png" />
   </div>
 )
 
