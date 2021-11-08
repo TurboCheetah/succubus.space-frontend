611789e9da8a696b0753932327829fed8a09af1d
turbo <me@turbo.ooo>
2021-11-07 20:11:29 -0500

    fix: remove unneeded code

--- public/js/main.js
+++ public/js/main.js
@@ -53,38 +53,6 @@
   })
 
   /* ------------------
-		Hero Slider
-	-------------------- */
-  var hero_s = $('.hero__slider')
-  hero_s.owlCarousel({
-    loop: true,
-    margin: 0,
-    items: 1,
-    dots: true,
-    nav: true,
-    navText: ["<span class='arrow_carrot-left'></span>", "<span class='arrow_carrot-right'></span>"],
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    smartSpeed: 1200,
-    autoHeight: false,
-    autoplay: true,
-    mouseDrag: false
-  })
-
-  /* ------------------
-        Video Player
-    -------------------- */
-  const player = new Plyr('#player', {
-    controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'captions', 'settings', 'fullscreen'],
-    seekTime: 25
-  })
-
-  /* ------------------
-        Niceselect
-    -------------------- */
-  $('select').niceSelect()
-
-  /* ------------------
         Scroll To Top
     -------------------- */
   $('#scrollToTopButton').click(function () {
